e393cd4eb9a837f9ceab11cd566ff503b2cbec32
orzoon <orzoonkunwar7.ak@gmail.com>
2019-10-31 17:14:46 +1100

    filessettup

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug')
+
+function cssStyles(){
+    return gulp.src('./scss/*.scss')
+           .pipe(sass())
+           .pipe(gulp.dest('./css'))
+           .pipe(browserSync.stream());
+}
+
+function pugFiles(){
+    return gulp.src('./pug/index.pug')
+           .pipe(pug())
+           .pipe(gulp.dest('./'))
+}
+
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    gulp.watch('./scss/*.scss', cssStyles)
+    gulp.watch('./pug/*.pug', pugFiles)
+    gulp.watch('index.html').on('change', browserSync.reload)
+}
+
+exports.cssStyles = cssStyles;
+exports.pugFiles = pugFiles;
+exports.watch = watch;
